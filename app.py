ea2e113e6665dbbe434dabdc07af9f5375f1f7d5
berry <spamnut@yeah.net>
2019-02-01 09:57:59 +0800

    Flask Init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ app.secret_key = b'\x0f\x1d\xe1\x98\xed\xc8n\x85\xf4\x95Cg\xdbGC\xf2'
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
